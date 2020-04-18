e9ee4629595ac0343206fcdc566edbc6c996d356
Moses <obikamoses@gmail.com>
2020-04-18 14:38:55 +0100

    Signin Form Add

--- lib/presentation/screens/sign_in_form.dart
+++ lib/presentation/screens/sign_in_form.dart
@@ -2,6 +2,8 @@ import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:note/injectable.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class SignInScreen extends StatelessWidget {
   @override
@@ -11,25 +13,99 @@ class SignInScreen extends StatelessWidget {
         title: Text("SignIn Form"),
       ),
       
-      body: BlocConsumer<SignInFormBloc, SignInFormState>(
-        listener: (context, state){
-          state.authFailureOrSuccessOption.fold(
-            () {}, 
-            (either){
-              either.fold((failure){
-                return Flushbar(
-                  message: failure.map(
-                    cancelledByUser: (_) => "CANCEL BY USER", 
-                    serverError: (_) => "System Clinch, Please Contant your administratir", 
-                    emailAlreadyInUse: (_) => 'Email already in use', 
-                    invalidEmailAndPasswordCombination: (_) =>
-                        'Invalid email and password combination'),
-                ).show(context);
-              }, 
-              (r) => (){});
-            });
-        },
-        builder: null, 
+      body: BlocProvider(
+        create: (context) => getIt<SignInFormBloc>(),
+        child: BlocConsumer<SignInFormBloc, SignInFormState>(
+          listener: (context, state){
+            state.authFailureOrSuccessOption.fold(
+              () {}, 
+              (either){
+                either.fold((failure){
+                  return Flushbar(
+                    message: failure.map(
+                      cancelledByUser: (_) => "CANCEL BY USER", 
+                      serverError: (_) => "System Clinch, Please Contant your administratir", 
+                      emailAlreadyInUse: (_) => 'Email already in use', 
+                      invalidEmailAndPasswordCombination: (_) =>
+                          'Invalid email and password combination'),
+                  ).show(context);
+                }, 
+                (r) => (){});
+              });
+          },
+          builder: (context, state){
+            return Form(
+              autovalidate: state.showErrorMessages,
+              child: ListView(
+                children: <Widget>[
+                  const Text(
+                  '📝',
+                  ).textAlignment(TextAlign.center).fontSize(130),
+                  const SizedBox(height: 8),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      labelText: 'Email',
+                    ),
+                    onChanged: (e){
+                      context.bloc<SignInFormBloc>().add(SignInFormEvent.emailChanged(e));
+                    },
+                    
+                    validator: (_) => state.emailAddress.value.fold(
+                      (f) => f.maybeMap(
+                        invalidEmail: (_) => 'Invalid email',
+                        orElse: () => null,
+                      ),
+                      (_) => null,
+                      
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                  // controller: passwordController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    autocorrect: false,
+                    onChanged: (value) => context.bloc<SignInFormBloc>().add(SignInFormEvent.passwordChanged(value)),
+                    validator: (_)=> state.password.value.fold(
+                      (f) => f.maybeMap(
+                        shortPassword: (_) => "Short Password",
+                        orElse: () => null
+                      ), 
+                      (r) => null
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: FlatButton(
+                          onPressed: () => context.bloc<SignInFormBloc>().add(
+                              const SignInFormEvent
+                                  .signInWithEmailAndPasswordPressed()),
+                          child: const Text('SIGN IN'),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: FlatButton(
+                          onPressed: () => context.bloc<SignInFormBloc>().add(
+                              const SignInFormEvent
+                                  .registerWithEmailAndPasswordPressed()),
+                          child: const Text('REGISTER'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            );
+          }, 
+        ),
       ),
     );
   }
